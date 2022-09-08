6faae2756a7d6f5d8e5a90a6d8201b8e2b6ba289
Michael Alberda <63167939+malberda@users.noreply.github.com>
2022-09-08 17:00:49 -0600

    Time: 95 ms (22.30%), Space: 45 MB (60.21%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int toret[] = new int[2];
         for(int i=0;i<nums.length;i++) {
-            for(int j=i;j<nums.length;j++) {
+            for(int j=i+1;j<nums.length;j++) {
                 if(nums[i]+nums[j]==target && i!=j) {
                     toret[0]= i;
                     toret[1]=j;
